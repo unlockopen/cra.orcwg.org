6ed4200274fa8023520fa850591ca6f0ec5d79db
ninnlangel <info@ninnlangel.com>
2025-09-19 13:34:39 +0200

    added orc and edit link

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
-module.exports = function(eleventyConfig) {
-  // Copy CSS files to output
-  eleventyConfig.addPassthroughCopy("src/css");
+module.exports = function (eleventyConfig) {
+  // Copy asset files to output
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   // Add markdown filter
-  eleventyConfig.addFilter("markdown", function(content) {
+  eleventyConfig.addFilter("markdown", function (content) {
     const markdownIt = require("markdown-it")();
     return markdownIt.render(content || "");
   });
 
   // Add category title filter
-  eleventyConfig.addFilter("categoryTitle", function(category) {
+  eleventyConfig.addFilter("categoryTitle", function (category) {
     return category
       .replace(/-/g, ' ')
       .replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
