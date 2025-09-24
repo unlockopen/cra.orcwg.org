534762302687a83c9e946e233ad8587547196e42
Tobie Langel <tobie@unlockopen.com>
2025-09-24 20:20:22 +0200

    Data refactoring

--- src/_data/faqItems.js
+++ src/_data/faqItems.js
@@ -1,18 +1,4 @@
-const faq = require('./faq.js');
-
+// Lightweight adapter for flat FAQ data (used for pagination)
 module.exports = function () {
-  const faqData = faq();
-  const items = [];
-
-  // Flatten the FAQ structure for pagination
-  for (const [category, questions] of Object.entries(faqData)) {
-    for (const question of questions) {
-      items.push({
-        ...question,
-        category
-      });
-    }
-  }
-
-  return items;
+  return require('./allContent.js')().faqItems;
 };
